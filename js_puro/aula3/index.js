344259ae3296513933db0a0a049b7942334053c3
Arthur Hoengen <arthurhoengen@gmail.com>
2023-12-13 16:53:30 -0300

    Inicializando projeto

---
+++ js_puro/aula3/index.js
@@ -0,0 +1,12 @@
+console.log('Hello World with "single quotes"'); // serve pra usar aspas duplas dentro
+console.log("Hello World with 'double quotes'"); // serve para usar aspas simples dentro
+console.log(`Hello world with crasis`); // usado para template string e funciona abas as aspas
+
+//javascript é tipagem dinâmica
+
+console.log(123456, 15213123123.8514121, 'Hello Again'); // se mandar mais de um valor, não quebra linha
+
+/* Tipos de dados vistos:
+String
+Number (não existe float, double, int. TUDO É NUMBER)
+*/
---
+++ js_puro/aula4/comentarios.js
@@ -0,0 +1,19 @@
+// Aula utilizada apenas para mostrar como funcionam comentarios
+console.log('Olá Mundo!') // Os comentarios são ignorados pela engine
+console.log('Linha 1')
+//console.log('Linha 2')
+/*
+console.log('Linha 3')
+console.log('Linha 4')
+*/
+
+// Vai ser ignorado
+// Node utiliza a mesma engine do Google Chrome
+
+
+/**
+ * Um título
+ * 
+ * Lorem ipsum kkkkkkk
+ * 
+ */
