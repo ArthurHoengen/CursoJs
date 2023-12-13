d146f4bdb9c9455a77115589fb728bfad4158348
Arthur Hoengen <arthurhoengen@gmail.com>
2023-12-13 20:01:06 -0300

    Finalizando aula 5

---
+++ js_puro/aula5/js/index.js
@@ -0,0 +1,5 @@
+console.log('Oi');
+console.log('Front end be like:');
+alert('Olá Mundo!'); //existe nos ambiente do navegador, mas não no node
+// navegador manipula o DOM
+// node manipula o backend
