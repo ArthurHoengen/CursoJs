6b9e5bee4108473096913134555f0bc0062d0145
Arthur Hoengen <arthurhoengen@gmail.com>
2024-01-04 10:40:56 -0300

    Finalizando aula 7

---
+++ js_puro/aula7/index.js
@@ -0,0 +1,20 @@
+// Constantes possuem as mesmas regras de variáveis
+// As regra adicionais são:
+// Não se pode modificar o valor de uma constante
+// Não se inicializa constante vazia
+
+const nome = 'João';
+console.log(nome);
+
+// + - * / % ^
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+// String = Texto | Number = Número
+
+console.log(resultado);
+
+console.log(typeof(primeiroNumero));
