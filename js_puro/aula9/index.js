e9f2dc597c1979f4e28ce64ed5821cf8783a5d4d
Arthur Hoengen <arthurhoengen@gmail.com>
2024-01-04 11:30:44 -0300

    Finalizando aula 9

---
+++ js_puro/aula9/index.js
@@ -0,0 +1,11 @@
+// Let vs Var - Primeira diferença
+
+// ECMAScript é uma padronização do javascript criado pela empresa ECMA
+var nome = 'Arthur';
+var nome = 'Otávio';
+
+console.log(nome);
+
+variavel = 'Qualquer coisa'; // NÃO FAÇA ISSO, SERÁ UMA VARIAVEL GLOBAL
+
+//Basicamente, sempre use let ou const ao invés de var
