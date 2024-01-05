7a893aebac8a69378792e4836b3777e69ce429b6
Arthur Hoengen <arthurhoengen@gmail.com>
2024-01-05 13:12:13 -0300

    Finalizando aula 12

---
+++ js_puro/aula12/js/script.js
@@ -0,0 +1,8 @@
+// alert, confirm e prompt (Navegador)
+// alert() - mensagem com o ok
+// confirm() - mensagem com o cancelar e ok
+// prompt() - mensagem com uma caixa de texto para o usuário
+
+const num1 = parseFloat(prompt('Digite um número'));
+const num2 = parseFloat(prompt('Digite outro número'));
+alert(`O resultado da sua conta foi: ${num1 + num2}`);
