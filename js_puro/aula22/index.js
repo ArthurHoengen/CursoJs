283bfc15732f23f6d0a05983c02caa936ca0703c
Arthur Hoengen <arthurhoengen@gmail.com>
2024-01-08 15:49:44 -0300

    Finalizando aula 22

---
+++ js_puro/aula22/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores de comparação
+>   Maior que
+>=  Maior ou igual a
+<   Menor que
+<=  Menor ou igual a
+==  Igualdade (valor) (não use)
+=== Igualdade estrita (valor e tipo)
+!=  Diferente (valor) (não use)
+!== Diferente estrito (valor e tipo)
+*/
+const num1 = 10;
+const num2 = '10';
+// const comparacao = num1 == num2; true
+const comparacao = num1 === num2;
+
+console.log(comparacao);
