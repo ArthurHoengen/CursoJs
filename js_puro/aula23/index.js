ca1fd4c76f134ab054a220bd08433be109306712
Arthur Hoengen <arthurhoengen@gmail.com>
2024-01-09 10:22:34 -0300

    Finalizando aula 23

---
+++ js_puro/aula23/index.js
@@ -0,0 +1,12 @@
+/* 
+Operadores lógicos
+&&  -> AND  -> E    -> Todas as expressões precisam ser verdadeiras
+||  -> OR   -> OU   -> Uma das expressões precisa ser verdadeira
+!   -> NOT  -> NÃO  -> Inverte uma expressão
+*/
+
+console.log(true && true);
+console.log(true && false);
+console.log(false || true);
+
+console.log(!false, !true);
