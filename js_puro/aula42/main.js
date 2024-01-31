b7ff09cfffc8b3322cf1f8c358913b7650d77801
Arthur Hoengen <arthurhoengen@gmail.com>
2024-01-31 13:08:54 -0300

    Finalizando aula 42

---
+++ js_puro/aula42/main.js
@@ -0,0 +1,18 @@
+// Break e Continue
+// Continue = pula pra próxima iteração do laço
+// Break = para a estrutura de repetição
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let numero of numeros) {
+  if (numero === 2 || numero === 5) {
+    console.log(`Pulei o número ${numero}`)
+    continue;
+  }
+
+  if (numero === 6) {
+    console.log(`Parei no número ${numero}`)
+    break;
+  }
+  console.log(numero);
+}
