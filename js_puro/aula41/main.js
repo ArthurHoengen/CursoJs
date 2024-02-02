93aa7120425c73af48c59043851c6eae68883430
Arthur Hoengen <arthurhoengen@gmail.com>
2024-02-02 11:23:09 -0300

    Finalizando aula 44 e formatando alguns arquivos

--- js_puro/aula41/main.js
+++ js_puro/aula41/main.js
@@ -10,15 +10,14 @@ const max = 50;
 
 let rand = 10;
 
- while(rand !== 10) {
-    rand = random(min, max);
-    console.log(rand);
+while (rand !== 10) {
+  rand = random(min, max);
+  console.log(rand);
 }
 
-console.log('######')
+console.log("######");
 
 do {
   rand = random(min, max);
   console.log(rand);
 } while (rand !== 10);
-
--- js_puro/aula42/main.js
+++ js_puro/aula42/main.js
@@ -6,12 +6,12 @@ const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (let numero of numeros) {
   if (numero === 2 || numero === 5) {
-    console.log(`Pulei o número ${numero}`)
+    console.log(`Pulei o número ${numero}`);
     continue;
   }
 
   if (numero === 6) {
-    console.log(`Parei no número ${numero}`)
+    console.log(`Parei no número ${numero}`);
     break;
   }
   console.log(numero);
