3db09a23ce8003940139ac0b1a3e670d692882b9
Arthur Hoengen <arthurhoengen@gmail.com>
2024-02-02 11:11:44 -0300

    Finalizando aula 43

---
+++ js_puro/aula43/main.js
@@ -0,0 +1,9 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+function max(num1, num2){
+    return num1 > num2 ? num1 : num2  // Meu jeito 
+}
+
+const max2 = (x,y) => x > y ? x : y // Com arrow function
+
+console.log(max(5,12));
