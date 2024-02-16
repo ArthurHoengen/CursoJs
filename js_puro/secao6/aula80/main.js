f851dab2bb60c99f7be7335758c8b313c7622258
Arthur Hoengen <arthurhoengen@gmail.com>
2024-02-16 15:14:35 -0300

    Finalizando aula 80 - Polimorfismo

---
+++ js_puro/secao6/aula80/main.js
@@ -0,0 +1,70 @@
+// Polimorfismo
+
+// Superclass
+function Conta(agencia, conta, saldo) {
+  this.agencia = agencia;
+  this.conta = conta;
+  this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function (valor) {
+  if (this.saldo < valor) {
+    console.log(`Saldo insuficiente: ${this.saldo}`);
+    return;
+  }
+
+  this.saldo -= valor;
+  this.verSaldo();
+};
+
+Conta.prototype.depositar = function (valor) {
+  this.saldo += valor;
+  this.verSaldo();
+};
+
+Conta.prototype.verSaldo = function () {
+  console.log(
+    `Ag/c.: ${this.agencia}/${this.conta} | ` +
+      `Saldo: R$${this.saldo.toFixed(2)}`
+  );
+};
+
+function ContaCorrente(agencia, conta, saldo, limite) {
+  Conta.call(this, agencia, conta, saldo);
+  this.limite = limite;
+}
+
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function (valor) {
+  if (this.saldo + this.limite < valor) {
+    console.log(`Saldo insuficiente: ${this.saldo}`);
+    return;
+  }
+
+  this.saldo -= valor;
+  this.verSaldo();
+};
+
+function ContaPoupanca(agencia, conta, saldo) {
+  Conta.call(this, agencia, conta, saldo);
+}
+
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.prototype.constructor = ContaPoupanca;
+
+const conta1 = new Conta(1, 11, 10);
+console.log(conta1);
+conta1.depositar(100);
+conta1.sacar(120);
+
+const cc = new ContaCorrente(11, 22, 0, 100);
+console.log(cc);
+cc.depositar(10);
+cc.sacar(100);
+
+const cp = new ContaPoupanca(22, 33, 0);
+console.log(cp);
+cp.depositar(10);
+cp.sacar(100);
