1055dac74e1f1769ab56116914faeaaf1218d3ee
Arthur Hoengen <arthurhoengen@gmail.com>
2024-02-27 11:39:04 -0300

    Finalizando aula 101 - Core-js e regenerator-runtime

---
+++ js_puro/secao8/aula101/src/main.js
@@ -0,0 +1,8 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime"; // Servem para fazer promises, async e await funcionarem em navegadores muito antigos
+
+import "./assets/css/style.css";
+
+import executa from "./modules/promises";
+
+executa();
